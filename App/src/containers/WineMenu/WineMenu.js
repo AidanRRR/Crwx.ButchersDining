c7a43b49348a8ab9185aae54bd8bc3a3f6d8c32a
Aidan <aidan.rypens@outlook.com>
2018-12-26 12:20:57 +0100

    Updated data

--- App/src/containers/WineMenu/WineMenu.js
+++ App/src/containers/WineMenu/WineMenu.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
+import {
+    StyleSheet, Text, View, ImageBackground, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback, ScrollView
+} from 'react-native';
 import styles from './Styles.js';
 
 import Data from './Data.json';
 
 export default class TypeMenu extends React.Component {
-
     state = {
         selection: {
             sizeType: 'BY THE GLASS',
@@ -25,7 +26,8 @@ export default class TypeMenu extends React.Component {
             selection: {
                 sizeType: this.state.selection.sizeType,
                 wineType: id
-            }
+            },
+            loading: true
         }, this.updateFilters);
     }
     changeSizeType = (id) => {
@@ -34,10 +36,11 @@ export default class TypeMenu extends React.Component {
                 sizeType: id,
                 wineType: this.state.selection.wineType
             },
+            loading: true
         }, this.updateFilters);
     }
 
-    updateFilters = () => {
+    updateFilters = async () => {
         const { selection } = this.state;
 
         let filtered;
@@ -54,84 +57,142 @@ export default class TypeMenu extends React.Component {
             })
         };
 
-        this.setState({ displayData: filtered[0] });
+        await this.setState({ displayData: filtered[0], loading: false });
     }
 
     handleBack = () => { }
 
     render() {
-        const { selection, displayData } = this.state;
+        const { selection, displayData, loading } = this.state;
 
-        const showWineTypes = (selection.sizeType.toUpperCase() === 'BY THE GLASS'.toUpperCase() ||
-            selection.sizeType.toUpperCase() === 'BOTTLES'.toUpperCase());
+        const byTheGlass = selection.sizeType.toUpperCase() === 'BY THE GLASS'.toUpperCase();
+        const bottles = selection.sizeType.toUpperCase() === 'BOTTLES'.toUpperCase();
+        const butchersBasement = selection.sizeType.toUpperCase() === 'THE BUTCHER\'S BASEMENT'.toUpperCase();
 
-        return (
-            <View style={styles.container}>
-                <View style={styles.leftNavbar}>
-                    <View style={{ marginBottom: 40 }}>
-                        <TouchableOpacity onPress={this.handleBack}>
-                            <Text style={styles.leftNavbarText}>
-                                TERUG
-                            </Text>
-                        </TouchableOpacity>
-                    </View>
-
-                    {showWineTypes && (
-                        <View>
-                            <TouchableOpacity onPress={() => { this.changeWineType('WIT'); }}>
-                                {selection.wineType === 'WIT' ?
-                                    (<Text style={[styles.leftNavbarText, styles.leftNavbarTextUnderline]}>WIT</Text>) :
-                                    (<Text style={[styles.leftNavbarText]}>WIT</Text>)}
-                            </TouchableOpacity>
-                            <TouchableOpacity onPress={() => { this.changeWineType('ROSÉ'); }}>
-                                {selection.wineType === 'ROSÉ' ?
-                                    (<Text style={[styles.leftNavbarText, styles.leftNavbarTextUnderline]}>ROSÉ</Text>) :
-                                    (<Text style={[styles.leftNavbarText]}>ROSÉ</Text>)}
-                            </TouchableOpacity>
-                            <TouchableOpacity onPress={() => { this.changeWineType('ROOD'); }}>
-                                {selection.wineType === 'ROOD' ?
-                                    (<Text style={[styles.leftNavbarText, styles.leftNavbarTextUnderline]}>ROOD</Text>) :
-                                    (<Text style={[styles.leftNavbarText]}>ROOD</Text>)}
+        if (loading) {
+            return (<Text>Loading</Text>)
+        } else {
+            return (
+                <View style={styles.container}>
+                    <View style={styles.leftNavbar}>
+                        <View style={{ marginBottom: 40 }}>
+                            <TouchableOpacity onPress={this.handleBack}>
+                                <Text style={styles.leftNavbarText}>
+                                    TERUG
+                                </Text>
                             </TouchableOpacity>
                         </View>
-                    )}
+                        {byTheGlass && (
+                            <View>
+                                <TouchableOpacity onPress={() => { this.changeWineType('WIT'); }}>
+                                    {selection.wineType === 'WIT' ?
+                                        (<Text style={[styles.leftNavbarText, styles.leftNavbarTextUnderline]}>WIT</Text>) :
+                                        (<Text style={[styles.leftNavbarText]}>WIT</Text>)}
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => { this.changeWineType('ROSÉ'); }}>
+                                    {selection.wineType === 'ROSÉ' ?
+                                        (<Text style={[styles.leftNavbarText, styles.leftNavbarTextUnderline]}>ROSÉ</Text>) :
+                                        (<Text style={[styles.leftNavbarText]}>ROSÉ</Text>)}
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => { this.changeWineType('ROOD'); }}>
+                                    {selection.wineType === 'ROOD' ?
+                                        (<Text style={[styles.leftNavbarText, styles.leftNavbarTextUnderline]}>ROOD</Text>) :
+                                        (<Text style={[styles.leftNavbarText]}>ROOD</Text>)}
+                                </TouchableOpacity>
+                            </View>
+                        )}
+                        {bottles && (
+                            <View>
+                                <TouchableOpacity onPress={() => { this.changeWineType('BUBBLES'); }}>
+                                    {selection.wineType === 'BUBBLES' ?
+                                        (<Text style={[styles.leftNavbarText, styles.leftNavbarTextUnderline]}>BUBBLES</Text>) :
+                                        (<Text style={[styles.leftNavbarText]}>BUBBLES</Text>)}
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => { this.changeWineType('WIT'); }}>
+                                    {selection.wineType === 'WIT' ?
+                                        (<Text style={[styles.leftNavbarText, styles.leftNavbarTextUnderline]}>WIT</Text>) :
+                                        (<Text style={[styles.leftNavbarText]}>WIT</Text>)}
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => { this.changeWineType('ROSÉ'); }}>
+                                    {selection.wineType === 'ROSÉ' ?
+                                        (<Text style={[styles.leftNavbarText, styles.leftNavbarTextUnderline]}>ROSÉ</Text>) :
+                                        (<Text style={[styles.leftNavbarText]}>ROSÉ</Text>)}
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => { this.changeWineType('ROOD'); }}>
+                                    {selection.wineType === 'ROOD' ?
+                                        (<Text style={[styles.leftNavbarText, styles.leftNavbarTextUnderline]}>ROOD</Text>) :
+                                        (<Text style={[styles.leftNavbarText]}>ROOD</Text>)}
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => { this.changeWineType('ZOET'); }}>
+                                    {selection.wineType === 'ZOET' ?
+                                        (<Text style={[styles.leftNavbarText, styles.leftNavbarTextUnderline]}>ZOET</Text>) :
+                                        (<Text style={[styles.leftNavbarText]}>ZOET</Text>)}
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => { this.changeWineType('SPECIAL'); }}>
+                                    {selection.wineType === 'SPECIAL' ?
+                                        (<Text style={[styles.leftNavbarText, styles.leftNavbarTextUnderline]}>SPECIAL</Text>) :
+                                        (<Text style={[styles.leftNavbarText]}>SPECIAL</Text>)}
+                                </TouchableOpacity>
+                            </View>
+                        )}
+                        {butchersBasement && (
+                            <View>
+                                <TouchableOpacity onPress={() => { this.changeWineType('WIT'); }}>
+                                    {selection.wineType === 'WIT' ?
+                                        (<Text style={[styles.leftNavbarText, styles.leftNavbarTextUnderline]}>WIT</Text>) :
+                                        (<Text style={[styles.leftNavbarText]}>WIT</Text>)}
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => { this.changeWineType('ROOD'); }}>
+                                    {selection.wineType === 'ROOD' ?
+                                        (<Text style={[styles.leftNavbarText, styles.leftNavbarTextUnderline]}>ROOD</Text>) :
+                                        (<Text style={[styles.leftNavbarText]}>ROOD</Text>)}
+                                </TouchableOpacity>
+                            </View>
+                        )}
+                    </View>
+                    <View style={styles.mainArea}>
+                        <View style={styles.topNavbar}>
+                            <TouchableOpacity onPress={() => { this.changeSizeType('BY THE GLASS'); }}>
+                                {selection.sizeType === 'BY THE GLASS' ?
+                                    (<Text style={[styles.topNavbarTextFirst, styles.topNavbarTextUnderline]}>BY THE GLASS</Text>) :
+                                    (<Text style={[styles.topNavbarTextFirst]}>BY THE GLASS</Text>)}
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => { this.changeSizeType('BOTTLES'); }}>
+                                {selection.sizeType === 'BOTTLES' ?
+                                    (<Text style={[styles.topNavbarText, styles.topNavbarTextUnderline]}>BOTTLES</Text>) :
+                                    (<Text style={[styles.topNavbarText]}>BOTTLES</Text>)}
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => { this.changeSizeType('THE BUTCHER\'S BASEMENT'); }}>
+                                {selection.sizeType === 'THE BUTCHER\'S BASEMENT' ?
+                                    (<Text style={[styles.topNavbarText, styles.topNavbarTextUnderline]}>THE BUTCHER'S BASEMENT</Text>) :
+                                    (<Text style={[styles.topNavbarText]}>THE BUTCHER'S BASEMENT</Text>)}
+                            </TouchableOpacity>
+                        </View>
+                        <ScrollView style={styles.wineList}>
+                            <View style={styles.ListTitle}>
+                                <Text style={styles.ListTitleText}>
+                                    {selection.wineType}
+                                </Text>
+                            </View>
+                            <View>
+                                {byTheGlass && (
+                                    this.renderByTheGlass(displayData)
+                                )}
+                                {bottles && (
+                                    this.renderBottles(displayData)
+                                )}
+                                {butchersBasement && (
+                                    this.renderButchersBasement(displayData)
+                                )}
+                            </View>
+                        </ScrollView>
+                    </View>
                 </View>
-                <View style={styles.mainArea}>
-                    <View style={styles.topNavbar}>
-                        <TouchableOpacity onPress={() => { this.changeSizeType('BY THE GLASS'); }}>
-                            {selection.sizeType === 'BY THE GLASS' ?
-                                (<Text style={[styles.topNavbarTextFirst, styles.topNavbarTextUnderline]}>BY THE GLASS</Text>) :
-                                (<Text style={[styles.topNavbarTextFirst]}>BY THE GLASS</Text>)}
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => { this.changeSizeType('BOTTLES'); }}>
-                            {selection.sizeType === 'BOTTLES' ?
-                                (<Text style={[styles.topNavbarText, styles.topNavbarTextUnderline]}>BOTTLES</Text>) :
-                                (<Text style={[styles.topNavbarText]}>BOTTLES</Text>)}
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => { this.changeSizeType('THE BUTCHER\'S BASEMENT'); }}>
-                            {selection.sizeType === 'THE BUTCHER\'S BASEMENT' ?
-                                (<Text style={[styles.topNavbarText, styles.topNavbarTextUnderline]}>THE BUTCHER'S BASEMENT</Text>) :
-                                (<Text style={[styles.topNavbarText]}>THE BUTCHER'S BASEMENT</Text>)}
-                        </TouchableOpacity>
-                    </View>
-                    <View style={styles.wineList}>
-                        <View style={styles.ListTitle}>
-                            <Text style={styles.ListTitleText}>
-                                {selection.wineType}
-                            </Text>
-                        </View>
-                        <View>
-                            {showWineTypes && (
-                                this.renderWithWineTypes(displayData)
-                            )}
-                        </View>
-                    </View>
-                </View>
-            </View>
-        );
+            );
+        }
     }
 
-    renderWithWineTypes = (displayData) => {
+    renderByTheGlass = (displayData) => {
         return (
             displayData.wines.map((wine, i) => {
                 return (
@@ -154,9 +215,65 @@ export default class TypeMenu extends React.Component {
                         </View>
                     </View>
                 );
-            }))
-    };
-    renderWithoutWineTypes = () => { };
+            })
+        )
+    }
+    renderBottles = (displayData) => {
+        return (
+            displayData.wines.map((country, i) => {
+                return (
+                    country.wines.map((wine, i) => {
+                        return (
+                            <View key={i} style={styles.ListItem}>
+                                <View style={styles.ItemTitleDescPrice}>
+                                    <Text style={styles.ItemTitle}>{wine.title}</Text>
+                                    <Text style={styles.TitleSeperator}> * </Text>
+                                    <Text style={styles.ItemRegion}>{wine.region}</Text>
+                                </View>
+                                <View style={styles.ListItemPrice}>
+                                    <Text style={styles.ItemPrice}>{wine.price}</Text>
+                                </View>
+                                <View>
+                                    <View>
+                                        <Text style={styles.ItemType}>{wine.region}</Text>
+                                    </View>
+                                    <View>
+                                        <Text style={styles.ItemDescription}>{wine.description}</Text>
+                                    </View>
+                                </View>
+                            </View>
+                        );
+                    })
+                )
+            })
+        )
+    }
+    renderButchersBasement = (displayData) => {
+        return (
+            displayData.wines.map((wine, i) => {
+                return (
+                    <View key={i} style={styles.ListItem}>
+                        <View style={styles.ItemTitleDescPrice}>
+                            <Text style={styles.ItemTitle}>{wine.title}</Text>
+                            <Text style={styles.TitleSeperator}> * </Text>
+                            <Text style={styles.ItemRegion}>{wine.region}</Text>
+                        </View>
+                        <View style={styles.ListItemPrice}>
+                            <Text style={styles.ItemPrice}>{wine.price}</Text>
+                        </View>
+                        <View>
+                            <View>
+                                <Text style={styles.ItemType}>{wine.region}</Text>
+                            </View>
+                            <View>
+                                <Text style={styles.ItemDescription}>{wine.description}</Text>
+                            </View>
+                        </View>
+                    </View>
+                );
+            })
+        )
+    }
 
     navigateToMenu = (menu) => {
         this.props.navigation.navigate(menu);
